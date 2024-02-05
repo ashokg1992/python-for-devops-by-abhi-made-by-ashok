da077a75095901c23798722dad7b563c1b1ae1a9
ashok <ashokreddyothers@gmail.com>
2024-02-06 01:59:39 +0530

    g

---
+++ 1.mynotes-from-this-repo/day-3.py
@@ -0,0 +1,18 @@
+#  for key-words : go to day-3 folder
+
+# # - local variables:
+#     - if we  declare variables inside a function, then those are local varialbes and those variables work oonly with in thta funciton 
+#     - 
+a=10        # global varialbes 
+b=20
+def addtion  ( ):
+    print (a+b)
+def sub ():
+    print (a-b)
+    
+addtion ()  # we call jsut those functions here
+sub ()
+
+
+#    +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
