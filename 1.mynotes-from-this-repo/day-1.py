1023ea6730fe175689f6161ce2ef09745cdd1fdb
ashok <ashokreddyothers@gmail.com>
2024-02-06 02:37:08 +0530

    g

---
+++ 1.mynotes-from-this-repo/day-1.py
@@ -0,0 +1,9 @@
+
+# =========================================
+# - by using shell , we can not configure/manage/fetch data by shell commands. For this, python or ansible is helpful.
+# - for complex PROGRAMMING, INTERACT WITH APIs ,   data manuplations, then we use python.
+ 
+
+# -in shell - curl we use for hit API, and we get data in JSON(jq.)
+# - in python - request - get data in json - datahype
+
