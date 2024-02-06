65c9c221c64bea9199b51c69b6e3c19e2aa95941
ashok <ashokreddyothers@gmail.com>
2024-02-07 04:32:22 +0530

    g

---
+++ 1.mynotes-from-this-repo/day-9.py
@@ -0,0 +1,15 @@
+# for i in range (10):
+#     print ("ashok")
+
+# ### ===================
+
+# for i in range (10):
+#     print ("ashok")
+#     print("kumar")
+
+
+ ### ===================
+ 
+colors = ["orange","green",'blue']
+for i in colors:
+    print (i)
